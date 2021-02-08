5a8719032e49c6b3475c4a6ebff0c853b51a7dac
syjeon <jsyu3799@gmail.com>
2021-02-09 00:14:12 +0900

    step08,09 업로드

---
+++ steps/step08.py
@@ -0,0 +1,80 @@
+import numpy as np
+
+
+class Variable:
+    def __init__(self, data):
+        self.data = data
+        self.grad = None
+        self.creator = None
+
+    def set_creator(self, func):
+        self.creator = func
+
+    def backward(self):
+        funcs = [self.creator]
+        while funcs:
+            f = funcs.pop()  # 함수를 가져온다.
+            x, y = f.input, f.output  # 함수의 입출력을 가져온다.
+            x.grad = f.backward(y.grad)  # backward 메서드를 호출한다.
+
+            if x.creator is not None:
+                funcs.append(x.creator)  # 하나 앞의 함수를 리스트에 추가한다.
+
+
+class Function:
+    def __call__(self, input):
+        x = input.data
+        y = self.forward(x)
+        output = Variable(y)
+        output.set_creator(self)  # 출력 변수에 창조자를 설정
+        self.input = input  # 입력변수 기억
+        self.output = output  # 출력도 저장
+        return output
+
+    def forward(self, x):
+        raise NotImplementedError()
+
+    def backward(self, gy):
+        raise NotImplementedError()
+
+
+class Sqaure(Function):
+    def forward(self, x):
+        return x ** 2
+
+    def backward(self, gy):
+        x = self.input.data
+        gx = 2 * x * gy
+        return gx
+
+
+class Exp(Function):
+    def forward(self, x):
+        return np.exp(x)
+
+    def backward(self, gy):
+        x = self.input.data
+        gx = np.exp(x) * gy
+        return gx
+
+
+def numerical_diff(f, x, eps=1e-4):
+    x0 = Variable(x.data - eps)
+    x1 = Variable(x.data + eps)
+    y0 = f(x0)
+    y1 = f(x1)
+    return (y1.data - y0.data) / (2 * eps)
+
+
+A = Sqaure()
+B = Exp()
+C = Sqaure()
+
+x = Variable(np.array(0.5))
+a = A(x)
+b = B(a)
+y = C(b)
+
+y.grad = np.array(1.0)
+y.backward()
+print(x.grad)
---
+++ steps/step09.py
@@ -0,0 +1,93 @@
+import numpy as np
+
+
+class Variable:
+    def __init__(self, data):
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError('{}은(는) 지원하지 않습니다. '.format(type(data)))
+
+        self.data = data
+        self.grad = None
+        self.creator = None
+
+    def set_creator(self, func):
+        self.creator = func
+
+    def backward(self):
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+
+        funcs = [self.creator]
+        while funcs:
+            f = funcs.pop()  # 함수를 가져온다.
+            x, y = f.input, f.output  # 함수의 입출력을 가져온다.
+            x.grad = f.backward(y.grad)  # backward 메서드를 호출한다.
+
+            if x.creator is not None:
+                funcs.append(x.creator)  # 하나 앞의 함수를 리스트에 추가한다.
+
+
+class Function:
+    def __call__(self, input):
+        x = input.data
+        y = self.forward(x)
+        output = Variable(as_array(y))
+        output.set_creator(self)  # 출력 변수에 창조자를 설정
+        self.input = input  # 입력변수 기억
+        self.output = output  # 출력도 저장
+        return output
+
+    def forward(self, x):
+        raise NotImplementedError()
+
+    def backward(self, gy):
+        raise NotImplementedError()
+
+
+class Sqaure(Function):
+    def forward(self, x):
+        return x ** 2
+
+    def backward(self, gy):
+        x = self.input.data
+        gx = 2 * x * gy
+        return gx
+
+
+class Exp(Function):
+    def forward(self, x):
+        return np.exp(x)
+
+    def backward(self, gy):
+        x = self.input.data
+        gx = np.exp(x) * gy
+        return gx
+
+
+def numerical_diff(f, x, eps=1e-4):
+    x0 = Variable(x.data - eps)
+    x1 = Variable(x.data + eps)
+    y0 = f(x0)
+    y1 = f(x1)
+    return (y1.data - y0.data) / (2 * eps)
+
+
+def sqaure(x):
+    return Sqaure()(x)
+
+
+def exp(x):
+    return Exp()(x)
+
+
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
+
+
+x = Variable(np.array(0.5))
+y = sqaure(exp(sqaure(x)))
+y.backward()
+print(x.grad)
